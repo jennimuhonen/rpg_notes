eda79d653f798de30b0042f01ef80b9ea0145a77
Jenni Muhonen <jenni@Otus>
2025-02-13 23:17:29 +0200

    ensimmäinen commit

---
+++ src/main/java/project/rpg_notes/RpgNotesApplication.java
@@ -0,0 +1,13 @@
+package project.rpg_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/rpg_notes/RpgNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package project.rpg_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
